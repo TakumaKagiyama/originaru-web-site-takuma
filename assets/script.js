89183f39d1f873406d37335e41c10765f95f24ed
TakumaKagiyama <tk04202580@gmail.com>
2025-04-28 09:10:56 +0800

    save

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,22 @@ document.getElementById('btn-en').addEventListener('click', function() {
   document.getElementById('text-ja').style.display = 'none';
   document.getElementById('text-en').style.display = 'block';
 });
+
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+});
+
+// メニュー内のリンクをクリックしたら閉じるようにする
+const menuLinks = document.querySelectorAll('#menu a');
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.style.display = 'none';
+  });
+});
